ad1a527acea4cd0ad5e73ecd0b164d3cc348bdf6
Reynolds <michaelreynoldk@gmail.com>
2021-02-23 22:25:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kertasbatuguntingapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kertasbatuguntingapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kertasbatuguntingapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
+
+    }
+}
